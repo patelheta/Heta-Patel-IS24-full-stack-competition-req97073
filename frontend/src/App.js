4b2250afb09d29c16ab848efceb2e4dea378f7fe
patelheta <patelheta385@gmail.com>
2023-03-27 12:17:45 -0700

    Updating app.js file

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+import ProductsList from './components/ProductsList';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ProductsList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
