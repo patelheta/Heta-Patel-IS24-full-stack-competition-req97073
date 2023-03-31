4a7cd2c97eed6adba703ef5d51aa817ed450eeb7
patelheta <patelheta385@gmail.com>
2023-03-30 23:59:25 -0700

    Add Swagger API documentation for backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const helmet = require('helmet');
 const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger.json');
 const { Products } = require('./Products');
 
+// A Function to generate a random Product ID
 const generateProductId = function() {
   return Math.floor(100000 + Math.random() * 900000);
 };
@@ -15,12 +17,17 @@ const PORT = 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan('combined'));
 
+// Swagger config
+app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Health end point 
 app.get('/api/health', (req, res) => {
   res.status(200).send('Application is Healthy');
 });
 
+// End point to get all products
 app.get('/api/products', (req, res) => {
   res.status(200).json(Products);
 });
@@ -31,45 +38,51 @@ app.get('/api/products', (req, res) => {
    "orderBy": "scrumMasterName" || "developer"
 }
  */
+// End point to search product by developer name or scrum master name
 app.get('/api/products/search', (req, res) => {
-  const inputValue = req.body.value;
-  const orderBy = req.body.orderBy;
-  let filterProducts;
-  if (orderBy === 'developer') {
-    filterProducts = Products.filter(product => product['Developers'].includes(inputValue));
-  } else if (orderBy === 'scrumMasterName') {
-    filterProducts = Products.filter(product => product['scrumMasterName'].includes(inputValue));
+  console.log(req.query);
+  const inputValue = req.query.value.toLowerCase();
+  const filterBy = req.query.filterBy;
+  let filterProducts = [];
+  if (filterBy === 'developer') {
+    console.log();
+    filterProducts = Products.filter(product => product['Developers'].some(name => name.toLowerCase().includes(inputValue)));
+  } else if (filterBy === 'scrumMasterName') {
+    filterProducts = Products.filter(product => product['scrumMasterName'].toLowerCase().includes(inputValue));
   }
+  console.log(filterProducts);
   res.status(200).json(filterProducts);
 });
 
+// End point to Create new product in to all products list
 app.post('/api/products', (req, res) => {
   const newProduct = req.body;
   const newProductId = generateProductId();
   newProduct['productId'] = newProductId;
   Products.push(newProduct);
-  res.status(201).json(newProduct);
+  res.status(201).json('Product Created successfully');
 });
 
+// End point to Update a product
 app.put('/api/products/:id', (req, res) => {
   const updateProduct = req.body;
-  const updateProductId = req.params.id;
-  updateProduct['productId'] = updateProductId;
-  const productIndex = Products.findIndex(product => product.productId === updateProductId);
-  if (productIndex) {
+  const productId = Number(req.params.id);
+  updateProduct["productId"] = productId;
+  const productIndex = Products.findIndex(product => product.productId === productId);
+  if (productIndex > -1) {
     Products.splice(productIndex, 1, updateProduct);
   } else {
     return res.status(404).send('Product Not Found');
   }
 
-  res.status(200).json(Products[productIndex]);
+  res.status(200).json('Product updated successfully');
 });
 
+// End point to delete a product 
 app.delete('/api/products/:id', (req, res) => {
-  const updateProductId = req.params.id;
-  const productIndex = Products.findIndex(product => product.productId === updateProductId);
-  console.log(productIndex);
-  if (productIndex >= 0) {
+  const productId = Number(req.params.id);
+  const productIndex = Products.findIndex(product => product.productId === productId);
+  if (productIndex > -1) {
     Products.splice(productIndex, 1);
   } else {
     return res.status(404).send('Product Not Found');
